e0963b0139d88419214548990fc0948ab7ab2a92
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-10 10:42:56 +0600

    feat: Display usesr profile picture and implement data mode toggle in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router";
 import { AuthContext } from "../Context/AuthContext";
 import toast from "react-hot-toast";
 
 const Navbar = () => {
-  const {user,LogOut}=use(AuthContext)
-  const navigate= useNavigate()
+  const { user, LogOut } = use(AuthContext);
+  const navigate = useNavigate();
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   const links = (
     <>
       <li>
@@ -16,13 +22,16 @@ const Navbar = () => {
       </li>
     </>
   );
-    const handleLogOut = () => {
+  const handleLogOut = () => {
     LogOut()
       .then(() => {
         toast.success("LogOut Successfully");
         navigate("/");
       })
       .catch();
+  };
+  const handleTheme = (checked) => {
+    setTheme(checked ? "dark" : "light");
   };
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -52,15 +61,70 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <Link to='/' className="hover:underline hover:text-blue-500 font-bold text-2xl">FinEase</Link>
+        <Link
+          to="/"
+          className="hover:underline hover:text-blue-500 font-bold text-2xl"
+        >
+          FinEase
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
-      <div className="navbar-end">
-        {
-          user?<button onClick={handleLogOut} className="btn btn-primary">Logout</button>:<Link to='/login' className="btn btn-primary">Login</Link>
-        }
+      <div className="navbar-end gap-3">
+        {user ? (
+          <div className="dropdown dropdown-end z-50">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-9 border-2 border-gray-300 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  referrerPolicy="no-referrer"
+                  src={user.photoURL}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu  menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+            >
+              <div className=" pb-3 border-b border-b-gray-200">
+                <li className="text-sm font-bold">{user.displayName}</li>
+                <li className="text-xs">{user.email}</li>
+              </div>
+              <li className="mt-3">
+                <Link to={"/profile"}>Profile</Link>
+              </li>
+              <div className="p-2">
+                <input
+                  onChange={(e) => handleTheme(e.target.checked)}
+                  type="checkbox"
+                  defaultChecked={localStorage.getItem("theme") === "dark"}
+                  className="toggle"
+                />
+              </div>
+              <li>
+                <button
+                  onClick={handleLogOut}
+                  className="btn rounded btn-primary text-white"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <Link
+            to={"/login"}
+            className="btn rounded btn-primary text-white"
+          >
+            {" "}
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
